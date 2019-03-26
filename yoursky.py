a3f834d6bc7ffebcb0559e5ece363b43238b545a
NickR23 <nickrich2624@gmail.com>
2019-03-25 20:14:50 -0400

    Finished proto

--- yoursky.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-webbrowser.open('http://www.fourmilab.ch/yoursky/')
-
-
--- yourskymech.py
+++ yourskymech.py
@@ -1,9 +1,43 @@
+import sys
 import mechanize as mech
-br = mech.Browser()
-res = br.open('http://www.fourmilab.ch/yoursky/')
-#print(res.read())
-#res = br.response()
-for form in br.forms():
-  print("Form name:", form.name)
-  print(form)
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
+print("ARGS:",str(sys.argv))
+br = mech.Browser()
+br.set_handle_robots(False)   # ignore robots
+res = br.open('http://www.fourmilab.ch/cgi-bin/Yoursky')
+
+link = br.find_link("Customise")
+br.follow_link(link)
+print("URL:",br.geturl())
+
+
+#Selects the form we want
+br.form = list(br.forms())[0]
+
+
+#Set dynimg so we just get an image instd of a map
+control = br.form.find_control("dynimg")
+control.items[0].selected=True
+
+#Set lat and lon values
+control = br.form.find_control("lat")
+control.value = sys.argv[1]
+
+control = br.form.find_control("lon")
+control.value = sys.argv[2]
+
+#submit form
+res = br.submit()
+
+#download generate img
+img = open('sky.jpg','wb')
+img.write(requests.get(br.geturl()).content)
+img.close()
+
+#display image
+img = mpimg.imread('sky.jpg')
+imgplot = plt.imshow(img)
+plt.show()
