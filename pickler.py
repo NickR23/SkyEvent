4b907c3333732e4e7c27fb8017cc7d1a4381ce90
NickR23 <nickrich2624@gmail.com>
2019-03-26 19:43:24 -0400

    Added map for flags

---
+++ pickler.py
@@ -0,0 +1,40 @@
+import pickle
+
+def save( obj, name ):
+  with open('obj/' + name + '.pkl', 'wb') as f:
+    pickle.dump( obj, f, pickle.HIGHEST_PROTOCOL)
+
+# def load( name ):
+#   with open('obj/' + name + '.pkl', 'rb') as f:
+#     return pickle.load(f)
+# w = load( 'hello' )
+# print(w)
+
+
+dict = {
+  '-d':'date',
+  '-u':'utc',
+  '-j':'jd',
+  '-l':'lat lon', #Signifies lat lon input next
+  '-n':'ns', #Direction of lat
+  '-e':'ew', #Direction of lon
+  '-c':'coords', #On/Off ecliptic and equator
+  '-m':'moonp', #On/Off moon and planets
+  '-d':'deep', #On/Off deep sky objects followed by magnitude value
+  '-co':'conto', #On/Off constellation outlines
+  '-cn':'constn', #OnOff constellation names
+  '-ca':'consta', #On/Off align names with horizon
+  '-cs':'consts', #On/Off abbreviate constellation names
+  '-cb':'constb', #On/Off show constellation bounds
+  '-sm':'limag', #Lower bound for mag of stars displayed
+  '-sn':'starn', #On/Off star names
+  '-snm':'starnm', #Lower bound for mag of named stars
+  '-sb':'starb', #On/Off for Bayer codes 
+  '-sbm':'starbm', #Lower bound for mag of Bayer labeled stars
+  '-i':'flip', #Invert South and North
+  '-is':'imgsize', #Set size of output image
+  '-f':'fontscale', #Set font size
+  '-co':'scheme', #Set scheme (colour, bw, wb, red)
+  }
+save( dict, 'flags' )
+
--- yourskymech.py
+++ yourskymech.py
@@ -1,35 +1,59 @@
+from spinner import Spinner
 import sys
 import mechanize as mech
 import requests
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
-
-print("ARGS:",str(sys.argv))
+spinner = Spinner() #Make spinner
+spinner.start()
+sys.stdout.flush()
+argc = len(sys.argv)
+print(argc,"ARGS:",str(sys.argv))
 br = mech.Browser()
 br.set_handle_robots(False)   # ignore robots
 res = br.open('http://www.fourmilab.ch/cgi-bin/Yoursky')
 
 link = br.find_link("Customise")
 br.follow_link(link)
-print("URL:",br.geturl())
-
+#print("URL:",br.geturl())
+spinner.stop()
 
 #Selects the form we want
 br.form = list(br.forms())[0]
 
 
+  
 #Set dynimg so we just get an image instd of a map
 control = br.form.find_control("dynimg")
 control.items[0].selected=True
+#Set color scheme
+control = br.form.find_control("scheme")
+control.value=["3"]
+#Disable star names
+br.form.find_control("starn").items[0].selected=False
+#Disable boundaries
+br.form.find_control("constb").items[0].selected=False
+#Disable names
+br.form.find_control("constn").items[0].selected=False
+#Disable coords
+br.form.find_control("coords").items[0].selected=False
 
 #Set lat and lon values
+lat = '0'
+lon = '0'
+if argc > 1:
+  lon = argv[1]
+if argc > 2:
+  lat = argv[2]
 control = br.form.find_control("lat")
-control.value = sys.argv[1]
+control.value = lat
 
 control = br.form.find_control("lon")
-control.value = sys.argv[2]
+control.value = lon
 
+for c in br.form.controls:
+  print(c)
 #submit form
 res = br.submit()
 
